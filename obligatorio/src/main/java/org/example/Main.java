62c3f31dffccb09126b25eaa65a7b69b55b0726d
Brian <brianbaldivieso@outlook.es>
2025-07-07 22:48:45 -0300

    Añaado mas metodos de regla, falta regla 3 y hacer bien el main, como se piden y muestran los datos

--- obligatorio/src/main/java/org/example/Main.java
+++ obligatorio/src/main/java/org/example/Main.java
@@ -153,7 +153,7 @@ public class Main {
 
     /**
      * METODO QUE VERIFICA LA REGLA UNO DE LA CONSIGNA
-     * Regla uno: "Debe de contener al menos una vocal.
+     * Regla uno: "Debe de contener al menos una vocal."
      */
     public static boolean reglaUno(String contrasenia){
         // Acá guardo en una variable la cantidad de caracteres que contiene la contraseña
@@ -161,7 +161,7 @@ public class Main {
         boolean seCumple = false; // En esta variable guardo si se cumplio la regla, la inicializo en false
 
         // Recorro la contraseña para ir verificando letra por letra
-        for (int i = 0; i < contrasenia.length(); i++) {
+        for (int i = 0; i < largoContraseña; i++) {
             // Verifico la letra obtenida y retorno el resultado
             if (esVocal(contrasenia.charAt(i))) {
                 return true;
@@ -169,6 +169,48 @@ public class Main {
         }
         return false;
     }
+
+    /**
+     * METODO QUE VERIFICA LA REGLA DOS DE LA CONSIGNA
+     * Regla dos: "No debe de tener tres vocales consecutivas o tres consonantes consecutivas"
+     */
+    public static boolean reglaDos(String contrasenia){
+        // Acá guardo en una variable la cantidad de caracteres que contiene la contraseña
+        final int largoContraseña = contrasenia.length();
+
+        //En estos dos contadores guardo la cantidad de vocales o consonantes seguidas tengo
+        int cantidadVocales = 0;
+        int cantidadConsonantes = 0;
+
+        // Recorro la contraseña
+        for (int i = 0; i < largoContraseña; i++) {
+            /*
+             * Obtengo el caracter de la posicion i dada por el bucle for y
+             * verifico si es vocal usando el metodo 'esVocal'
+             */
+            if (esVocal(contrasenia.charAt(i))) {
+                /*
+                 * Sumo uno al contador y reinicio el contador de consonantes
+                 */
+                cantidadVocales++;
+                cantidadConsonantes = 0;
+            } else {
+                /*
+                 * Sumo uno al contador y reinicio el contador de vocales
+                 */
+                cantidadConsonantes++;
+                cantidadVocales = 0;
+            }
+            /* Verifico que ninguno de los contadores haya llegado a 3,
+             * ya que significaria que hay 3 vocales o 3 consonantes seguidas.
+             */
+            if (cantidadConsonantes == 3 || cantidadVocales == 3) {
+                return false; // Se rompio la regla, asi que retorno false.
+            }
+        }
+        return true; // Si llego acá se no se rompio la regla, así que retorno true.
+    }
+
 
     /**
      * METODO PRINCIPAL DONDE SE EJECUTA LA LOGICA DEL PROGRAMA.
@@ -191,7 +233,10 @@ public class Main {
             contraseniaIngresada = contraseniasIngresadas.get(i);
             reglaUno = reglaUno(contraseniaIngresada);
             if (reglaUno){
-                System.out.println(contraseniaIngresada + " es váldia");
+                System.out.println(contraseniaIngresada + " cumple regla uno");
+            }
+            if (reglaDos(contraseniaIngresada)){
+                System.out.println(contraseniaIngresada + " cumple regla dos");
             }
         }
 
